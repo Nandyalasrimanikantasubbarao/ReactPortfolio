50b44962fb9f90317754cd7a297ead05db8e1971
Nandyala Sri Manikanta Subbarao <22h45a6105@bvcits.edu.in>
2025-02-08 17:55:11 +0530

    added to files

--- LatestPortfolio/Frontend/src/pages/Home.jsx
+++ LatestPortfolio/Frontend/src/pages/Home.jsx
@@ -1,5 +1,20 @@
+import ParticlesComponent from "../Componets/ParticlesComponent";
+import "./Home.css";
+
 function Home() {
-  return <div>hi</div>;
+  return (
+    <>
+      <ParticlesComponent id="particles" />
+      <div className="h-100% grid min-h-full grid-cols-2 place-items-center">
+        <div className="flex items-center justify-center">
+          <h1>Manikanta</h1>
+        </div>
+        <div>
+          <h1>hello</h1>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Home;
