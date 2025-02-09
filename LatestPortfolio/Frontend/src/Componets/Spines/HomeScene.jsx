9d9535fcf7c4a4033be1279e6169920d319cfe61
Nandyala Sri Manikanta Subbarao <22h45a6105@bvcits.edu.in>
2025-02-09 14:22:44 +0530

    added spline

---
+++ LatestPortfolio/Frontend/src/Componets/Spines/HomeScene.jsx
@@ -0,0 +1,7 @@
+import Spline from "@splinetool/react-spline";
+
+export default function HomeScene() {
+  return (
+    <Spline scene="https://prod.spline.design/0q0Zc981rYJ7QO7K/scene.splinecode" />
+  );
+}
--- LatestPortfolio/Frontend/src/pages/Home.jsx
+++ LatestPortfolio/Frontend/src/pages/Home.jsx
@@ -1,18 +1,21 @@
-import ParticlesComponent from "../Componets/ParticlesComponent";
+import HomeScene from "../Componets/Spines/HomeScene";
 import "./Home.css";
 
 function Home() {
   return (
     <>
-      <ParticlesComponent id="particles" />
-      <div className="h-100% grid min-h-full grid-cols-2 place-items-center">
-        <div className="flex items-center justify-center">
+      <div className="grid min-h-full grid-cols-2 place-items-center">
+        {/* <div className="flex items-center justify-center">
+          <div className="grid grid-cols-2 p-12"> */}
+        <div>
           <h1>Manikanta</h1>
         </div>
-        <div>
-          <h1>hello</h1>
+        <div className="absolute top-10">
+          <HomeScene />
         </div>
       </div>
+      {/* </div>
+      </div> */}
     </>
   );
 }
